5bcdf2b83823baf60172ca78272e28ddd6d2abc2
harshtaliwal30 <harshtaliwal@gmail.com>
2021-09-27 22:23:35 +0530

    Added project directory

---
+++ lib/screens/events_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EventsScreen extends StatelessWidget {
+  const EventsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Events Screen\nNeed to be done",
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/team_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TeamScreen extends StatelessWidget {
+  const TeamScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Team Screen\nNeed to be done",
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
