5126449326b2fa3559ccb7a4258f5d76afa15ef1
Shash0501 <75673036+Shash0501@users.noreply.github.com>
2021-10-01 23:26:28 +0530

    Created UI for TeamScreen (#16)
    * Created UI for TeamScreen
    
    * Made UI dynamic, replaced SizedBox with Padding
    
    * Formatted the team_screen.dart file
    
    * Made the changes requested
    
    Co-authored-by: shashank <shash.201cs257@gmail.com>

--- lib/screens/team_screen.dart
+++ lib/screens/team_screen.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:mlsa_amu/models/team_details.dart';
+import 'package:mlsa_amu/widgets/member_details_card.dart';
 
 class TeamScreen extends StatelessWidget {
   const TeamScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Team Screen\nNeed to be done",
-          textAlign: TextAlign.center,
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomLeft,
+          colors: [
+            Colors.blue.shade900,
+            Color(0XFF792adc),
+            Color(0XFF792adc),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          minimum: EdgeInsets.only(top: width * 0.241),
+          child: ListView.builder(
+            itemCount: memberDetails.length,
+            itemBuilder: (context, index) {
+              return MemberDetailsCard(memberdetails: memberDetails[index]);
+            },
+          ),
         ),
       ),
     );
---
+++ lib/widgets/member_details_card.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:mlsa_amu/models/team_details.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MemberDetailsCard extends StatelessWidget {
+  MemberDetailModel memberdetails;
+  MemberDetailsCard({required this.memberdetails});
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Padding(
+      padding: EdgeInsets.all(width * 0.0254),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        height: height * 0.26,
+        width: width * 0.9,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(
+              width * 0.0254, width * 0.01, width * 0.0254, width * 0.0254),
+          child: Row(
+            children: <Widget>[
+              Image.asset(
+                memberdetails.assetImage,
+                width: width * 0.4583,
+                height: height * 0.249,
+              ),
+              Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        EdgeInsets.fromLTRB(0, width * 0.012, 0, width * 0.006),
+                    child: Text(
+                      memberdetails.name,
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, height * 0.1),
+                    child: Text(
+                      memberdetails.position,
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 0, width * 0.051, 0),
+                        child: IconButton(
+                          icon: Icon(FontAwesomeIcons.githubSquare),
+                          iconSize: 40,
+                          onPressed: () {
+                            _launchURLBrowser(memberdetails.githubUrl);
+                          },
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(FontAwesomeIcons.linkedin),
+                        color: Colors.blue[700],
+                        iconSize: 40,
+                        onPressed: () {
+                          _launchURLBrowser(memberdetails.linkedinUrl);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+_launchURLBrowser(String url) async {
+  await launch(url);
+}
