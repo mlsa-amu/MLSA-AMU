ddde6037fbd90b80326639e92fc964b4e6fb2266
AgungPrabowo <agungprabowo112@gmail.com>
2021-10-03 03:46:21 +0700

    using media query for static value

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mlsa_amu/models/events_images.dart';
+import 'package:mlsa_amu/utils/size_config.dart';
 
 class EventsScreen extends StatelessWidget {
   const EventsScreen({Key? key}) : super(key: key);
@@ -32,29 +33,40 @@ class EventsScreen extends StatelessWidget {
         ),
         body: Center(
           child: ListView.builder(
-            padding: EdgeInsets.only(top: 20, bottom: 20),
+            padding: EdgeInsets.symmetric(
+              vertical: SizeConfig.safeBlockVertical * 3,
+              horizontal: SizeConfig.safeBlockHorizontal * 3,
+            ),
             itemBuilder: (BuildContext context, int index) {
               return Padding(
-                padding: EdgeInsets.only(bottom: 25),
+                padding: EdgeInsets.only(
+                  bottom: SizeConfig.safeBlockVertical * 3,
+                ),
                 child: Stack(children: [
                   ClipRRect(
                     borderRadius: BorderRadius.circular(20),
                     child: ColorFiltered(
                       colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.6), BlendMode.dstATop),
+                        Colors.black.withOpacity(0.6),
+                        BlendMode.dstATop,
+                      ),
                       child: Image.asset(
-                          "assets/images/${eventsImages[index].image}"),
+                        "assets/images/${eventsImages[index].image}",
+                      ),
                     ),
                   ),
                   Positioned(
-                    bottom: 20,
-                    left: 20,
+                    bottom: SizeConfig.safeBlockVertical * 3,
+                    left: SizeConfig.safeBlockHorizontal * 6,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           eventsImages[index].title,
-                          style: TextStyle(color: Colors.white, fontSize: 24),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: SizeConfig.baseFontSize * 6.6,
+                          ),
                         ),
                         Text(
                           eventsImages[index].subTitle,
