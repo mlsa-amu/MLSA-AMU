3f2803acefca4adaec3be12038f13599433ffc29
Agung Prabowo <agungprabowo112@gmail.com>
2021-10-03 05:13:39 +0700

    Events page (#19)
    * added events images model
    
    * added images events
    
    * display images events
    
    * adjust gradient color
    
    * using media query for static value

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mlsa_amu/models/events_images.dart';
+import 'package:mlsa_amu/utils/size_config.dart';
 
 class EventsScreen extends StatelessWidget {
   const EventsScreen({Key? key}) : super(key: key);
@@ -30,9 +32,53 @@ class EventsScreen extends StatelessWidget {
           elevation: 0,
         ),
         body: Center(
-          child: Text(
-            "Events Screen\nNeed to be done",
-            textAlign: TextAlign.center,
+          child: ListView.builder(
+            padding: EdgeInsets.symmetric(
+              vertical: SizeConfig.safeBlockVertical * 3,
+              horizontal: SizeConfig.safeBlockHorizontal * 3,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: EdgeInsets.only(
+                  bottom: SizeConfig.safeBlockVertical * 3,
+                ),
+                child: Stack(children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: ColorFiltered(
+                      colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.6),
+                        BlendMode.dstATop,
+                      ),
+                      child: Image.asset(
+                        "assets/images/${eventsImages[index].image}",
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: SizeConfig.safeBlockVertical * 3,
+                    left: SizeConfig.safeBlockHorizontal * 6,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          eventsImages[index].title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: SizeConfig.baseFontSize * 6.6,
+                          ),
+                        ),
+                        Text(
+                          eventsImages[index].subTitle,
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  )
+                ]),
+              );
+            },
+            itemCount: eventsImages.length,
           ),
         ),
       ),
