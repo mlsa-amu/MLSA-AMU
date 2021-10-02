9cf8354806440fa361bfb5620387ee08973493ef
Priya Nagda <64613009+pri1311@users.noreply.github.com>
2021-10-02 22:27:07 +0530

    [#15] Enhancement in events screen app bar (#20)
    * Enhancement in events screen app bar
    
    * remove unwanted properties and styling
    
    Co-authored-by: Priya Nagda <pri1311@users.noreply.github.com>

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class EventsScreen extends StatelessWidget {
@@ -5,44 +7,47 @@ class EventsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size(MediaQuery.of(context).size.width, 150),
-        child: Container(
-          padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-          child: Padding(
-            padding: EdgeInsets.only(left: 20, top: 5, bottom: 5, right: 10),
-            child: Row(
-              children: [
-                Text(
-                  "Events",
-                  style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                  ),
-                ),
-                Spacer(),
-                Image.asset(
-                  "assets/images/mlsa-logo.png",
-                  height: 50,
-                )
-              ],
-            ),
-          ),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xFF219653), Color(0xFF792ADC)],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomLeft,
+          colors: [
+            Colors.blue.shade900,
+            Color(0XFF792adc),
+            Color(0XFF792adc),
+          ],
         ),
       ),
-      body: Center(
-        child: Text(
-          "Events Screen\nNeed to be done",
-          textAlign: TextAlign.center,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text(
+            "Events",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          actions: [
+            Container(
+              margin: const EdgeInsets.all(8),
+              child: Image.asset(
+                "assets/images/mlsa-logo.png",
+                height: min(width * 0.14, height * 0.070),
+                width: min(width * 0.14, height * 0.070),
+              ),
+            )
+          ],
+        ),
+        body: Center(
+          child: Text(
+            "Events Screen\nNeed to be done",
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
