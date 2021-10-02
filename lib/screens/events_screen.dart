b55d3ee3fcd09605e661fa142243deeb1f4e672e
Priya Nagda <pri1311@users.noreply.github.com>
2021-10-02 22:07:25 +0530

    remove unwanted properties and styling

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -7,44 +7,43 @@ class EventsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
-    var width = MediaQuery.of(context).size.width;
-    print(width * 0.13);
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        toolbarHeight: height * 0.075,
-        title: const Text(
-          "Events",
-          style: TextStyle(
-            fontSize: 26,
-            fontWeight: FontWeight.w500,
-            color: Colors.white,
-          ),
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomLeft,
+          colors: [
+            Colors.blue.shade900,
+            Color(0XFF792adc),
+            Color(0XFF792adc),
+          ],
         ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-          Container(
-            margin: const EdgeInsets.all(8),
-            child: Image.asset(
-              "assets/images/mlsa-logo.png",
-              height: min(width * 0.14, height * 0.070),
-              width: min(width * 0.14, height * 0.070),
-              
-            ),
-          )
-        ],
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFF219653), Color(0xFF792ADC)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text(
+            "Events",
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          actions: [
+            Container(
+              margin: const EdgeInsets.all(8),
+              child: Image.asset(
+                "assets/images/mlsa-logo.png",
+                height: min(width * 0.14, height * 0.070),
+                width: min(width * 0.14, height * 0.070),
+              ),
+            )
+          ],
         ),
-        child: Center(
+        body: Center(
           child: Text(
             "Events Screen\nNeed to be done",
             textAlign: TextAlign.center,
