05efd4c4c1b8b4d40209c8f9d65568e32da0897f
rutuja1121 <patilrutuja1021@gmail.com>
2021-10-06 23:11:16 +0530

    appbar ui added

--- lib/screens/contributors_screen.dart
+++ lib/screens/contributors_screen.dart
@@ -5,11 +5,28 @@ class ContributorsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Contributors Screen\nNeed to be done",
-          textAlign: TextAlign.center,
+    return Container(
+      decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromARGB(240, 33, 150, 83),
+          Color.fromARGB(240, 121, 42, 220)
+        ],
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text('Contributors'),
+        ),
+        body: Center(
+          child: Text(
+            "Contributors Screen\nNeed to be done",
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
