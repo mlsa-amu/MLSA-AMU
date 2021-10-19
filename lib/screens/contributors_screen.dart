240d97bdf53a857e2e66ce592e6bd2a5c05078b6
harshtaliwal30 <harshtaliwal@gmail.com>
2021-10-19 22:35:45 +0530

    Contributors screen added

--- lib/screens/contributors_screen.dart
+++ lib/screens/contributors_screen.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:mlsa_amu/utils/size_config.dart';
+import 'package:mlsa_amu/widgets/contributors_details_card.dart';
 
 class ContributorsScreen extends StatelessWidget {
   const ContributorsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Contributors Screen\nNeed to be done",
-          textAlign: TextAlign.center,
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomLeft,
+          colors: [
+            Colors.blue.shade900,
+            Color(0XFF792adc),
+            Color(0XFF792adc),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Text(
+            'Contributors',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.transparent,
+        ),
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              ListView.separated(
+                separatorBuilder: (_, __) => SizedBox(
+                  height: 5,
+                ),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return ContributorDetailsCard();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/contributors_details_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:mlsa_amu/models/team_details.dart';
+import 'package:mlsa_amu/utils/size_config.dart';
+
+class ContributorDetailsCard extends StatelessWidget {
+  final MemberDetailModel? memberdetails;
+  ContributorDetailsCard({this.memberdetails});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.symmetric(
+        horizontal: SizeConfig.safeBlockHorizontal * 4,
+      ),
+      leading: CircleAvatar(
+        radius: SizeConfig.iconGeneralHeightAndWidth,
+        backgroundColor: Colors.white,
+      ),
+      title: Padding(
+        padding: EdgeInsets.only(bottom: SizeConfig.safeBlockVertical * 0.6),
+        child: Text(
+          "Harsh",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      subtitle: Text(
+        'Contributions: ' + "5",
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
